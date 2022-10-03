3c5d268732bbbaa2aeaa6496b92055e595f08ca2
Kouki <108082759+kourida-22@users.noreply.github.com>
2022-10-03 10:13:12 +0900

    Update 06_A.py

---
+++ AOJ/06/06_A.py
@@ -0,0 +1,11 @@
+org_list = [1, 2, 3, 4, 5]
+
+reverse_iterator reversed =(org_list)
+print(org_list)
+
+print(type(reverse iterator))
+for i in reversed(org_list):
+    print(i)
+new list = list(reversed(org_list))
+print(org_list)
+print(new_list)
