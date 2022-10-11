5e8d0d694e0faaae75120b1f2d38248ad0e7a545
Kouki <108082759+kourida-22@users.noreply.github.com>
2022-10-11 23:54:33 +0900

    Update 06_A.py

--- AOJ/06/06_A.py
+++ AOJ/06/06_A.py
@@ -1,12 +1,4 @@
-org_list = [1, 2, 3, 4, 5]
-
-reverse_iterator reversed =(org_list)
-print(org_list)
-
-print(type(reverse iterator))
-for i in reversed(org_list):
-    print(i)
-new list = list(reversed(org_list))
-print(org_list)
-print(new_list)
-
+n = int(input())
+get_lst = list(map(int, input().split()))
+get_lst.reverse()
+print(*get_lst)
