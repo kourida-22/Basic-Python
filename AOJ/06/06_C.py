b517fdac11b1cc0650c532d269a7c3e2e1eec735
Kouki <108082759+kourida-22@users.noreply.github.com>
2022-10-15 21:31:36 +0900

    Update 06_C.py

--- AOJ/06/06_C.py
+++ AOJ/06/06_C.py
@@ -1,2 +1,12 @@
+house = [[[0 for r in range(10)] for j in range(3)] for b in range(4)]
 n = int(input())
-count = [[[0 for i in range(10)] for j in range(3)] for k in range(4)]
+
+for i in range(n):
+    b, f, r, v = map(int, input().split())
+    houses[b - 1][f - 1][r - 1] += v
+    
+for b in range(4):
+    for f in houses[b]
+        print('', *f)
+    if b ! = 3:
+        print('#' * 20)
