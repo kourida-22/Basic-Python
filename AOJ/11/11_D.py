0d9290da0840ebfa30cc0819e7b5f5c478de8edc
Kouki <108082759+kourida-22@users.noreply.github.com>
2022-11-03 12:49:24 +0900

    Update 11_D.py

---
+++ AOJ/11/11_D.py
@@ -0,0 +1,16 @@
+n = int(input())
+surfaces_stack = [None] * n
+for i in range(n):
+    surfaces_stack[i] = list(map(int,input().split()))
+
+for i in range(n-1):
+    for j in range(i+1, n):
+        dice = Dice(surfaces_stack[i])
+        if dice.is_same_dice(surfaces_stack[j]):
+            print('No')
+            break
+    else:
+        continue
+    break
+else:
+    print('Yes')
