8ef978f875b200270c622e362e64a1ece2337d51
Kouki <108082759+kourida-22@users.noreply.github.com>
2022-11-12 15:42:13 +0900

    Update 11_A.py

--- AOJ/11/11_A.py
+++ AOJ/11/11_A.py
@@ -6,10 +6,14 @@ dice = Dice(surfaces)
 for inst in instructions:
     if inst == "E":
         dice.turn_e()
+        
     if inst == "N":
         dice.turn_n()
+        
     if inst == "S":
         dice.turn_s()
+        
     if inst == "W":
         dice.turn_w()
+        
 print(dice.show_top())
